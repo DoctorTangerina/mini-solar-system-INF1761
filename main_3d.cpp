3cfffdf5924e56b47affaad7d2a3cddd3152c27c
DoctorTangerina <arthursardellajiujitsu@gmail.com>
2025-10-21 16:24:18 -0300

    init commit projeto 2

---
+++ main_3d.cpp
@@ -0,0 +1,197 @@
+
+#ifdef _WIN32
+#define GLAD_GL_IMPLEMENTATION // Necessary for headeronly version.
+#include <glad/gl.h>
+#elif __APPLE__
+#include <OpenGL/gl3.h>
+#endif
+#include <GLFW/glfw3.h>
+
+#include "arcball.h"
+#include "scene.h"
+#include "state.h"
+#include "camera3d.h"
+#include "material.h"
+#include "texture.h"
+#include "transform.h"
+#include "quad.h"
+#include "sphere.h"
+#include "error.h"
+#include "shader.h"
+#include "light.h"
+#include "polyoffset.h"
+
+#include <iostream>
+#include <cassert>
+
+static float viewer_pos[3] = {2.0f, 3.5f, 4.0f};
+
+static ScenePtr scene;
+static Camera3DPtr camera;
+static ArcballPtr arcball;
+
+static void initialize (void)
+{
+  // set background color: white 
+  glClearColor(1.0f,1.0f,1.0f,1.0f);
+  // enable depth test 
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_CULL_FACE);  // cull back faces
+
+  // create objects
+  camera = Camera3D::Make(viewer_pos[0],viewer_pos[1],viewer_pos[2]);
+  //camera->SetOrtho(true);
+  arcball = camera->CreateArcball();
+
+  //LightPtr light = ObjLight::Make(viewer_pos[0],viewer_pos[1],viewer_pos[2]);
+  LightPtr light = Light::Make(0.0f,0.0f,0.0f,1.0f,"camera");
+
+  AppearancePtr white = Material::Make(1.0f,1.0f,1.0f);
+  AppearancePtr tex_earth = Texture::Make("decal", "./images/earth.jpg");
+
+  // create shader
+  ShaderPtr shader = Shader::Make(light, "camera");
+  shader->AttachVertexShader("./shaders/ilum_vert/vertex.glsl");
+  shader->AttachFragmentShader("./shaders/ilum_vert/fragment.glsl");
+  shader->Link();
+
+  // Define a different shader for texture mapping
+  // An alternative would be to use only this shader with a "white" texture for untextured objects
+  ShaderPtr shd_tex = Shader::Make(light, "camera");
+  shd_tex->AttachVertexShader("./shaders/ilum_vert/vertex_texture.glsl");
+  shd_tex->AttachFragmentShader("./shaders/ilum_vert/fragment_texture.glsl");
+  shd_tex->Link();
+
+  TransformPtr trf_table = Transform::Make();
+  trf_table->Scale(3.0f,0.3f,3.0f);
+  trf_table->Translate(0.0f,-1.0f,0.0f);
+
+  TransformPtr trf_box = Transform::Make();
+  trf_box->Scale(1.f,.5f,1.f);
+  
+  TransformPtr trf_ball = Transform::Make();
+  trf_ball->Scale(.2f, .2f, .2f);
+  trf_ball->Translate(-0.8f, 3.5f, -0.8f);
+
+  TransformPtr trf_can = Transform::Make();
+  trf_can->Scale(.2f, .2f, .2f);
+  trf_can->Translate(0.8f, 3.5f, 0.8f);
+
+  TransformPtr trf_earth = Transform::Make();
+  trf_earth->Scale(.5f, .5f, .5f);
+  trf_earth->Translate(-2.f, 1.f, -2.f);
+
+  TransformPtr trf_wood = Transform::Make();
+  trf_wood->Scale(0.4f, 0.4f, 0.4f);
+  trf_wood->Translate(2.5f, 1.f, 2.5f);
+
+  Error::Check("before shps");
+  //Error::Check("before quad");
+  //ShapePtr quad = Quad::Make();
+  Error::Check("before sphere");
+  ShapePtr sphere = Sphere::Make();
+  Error::Check("after shps");
+
+  // build scene
+
+  auto earth = Node::Make();
+
+  NodePtr root = Node::Make(shader, {earth});
+  scene = Scene::Make(root);
+}
+
+static void display (GLFWwindow* win)
+{ 
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear window 
+  Error::Check("before render");
+  scene->Render(camera);
+  Error::Check("after render");
+}
+
+static void error (int code, const char* msg)
+{
+  printf("GLFW error %d: %s\n", code, msg);
+  glfwTerminate();
+  exit(0);
+}
+
+static void keyboard (GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+static void resize (GLFWwindow* win, int width, int height)
+{
+  glViewport(0,0,width,height);
+}
+
+static void cursorpos (GLFWwindow* win, double x, double y)
+{
+  // convert screen pos (upside down) to framebuffer pos (e.g., retina displays)
+  int wn_w, wn_h, fb_w, fb_h;
+  glfwGetWindowSize(win, &wn_w, &wn_h);
+  glfwGetFramebufferSize(win, &fb_w, &fb_h);
+  x = x * fb_w / wn_w;
+  y = (wn_h - y) * fb_h / wn_h;
+  arcball->AccumulateMouseMotion(int(x),int(y));
+}
+static void cursorinit (GLFWwindow* win, double x, double y)
+{
+  // convert screen pos (upside down) to framebuffer pos (e.g., retina displays)
+  int wn_w, wn_h, fb_w, fb_h;
+  glfwGetWindowSize(win, &wn_w, &wn_h);
+  glfwGetFramebufferSize(win, &fb_w, &fb_h);
+  x = x * fb_w / wn_w;
+  y = (wn_h - y) * fb_h / wn_h;
+  arcball->InitMouseMotion(int(x),int(y));
+  glfwSetCursorPosCallback(win, cursorpos);     // cursor position callback
+}
+static void mousebutton (GLFWwindow* win, int button, int action, int mods)
+{
+  if (action == GLFW_PRESS) {
+    glfwSetCursorPosCallback(win, cursorinit);     // cursor position callback
+  }
+  else // GLFW_RELEASE 
+    glfwSetCursorPosCallback(win, nullptr);      // callback disabled
+}
+
+int main ()
+{
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,1);
+  glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);       // required for mac os
+  glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER,GLFW_TRUE);  // option for mac os
+#endif
+
+  glfwSetErrorCallback(error);
+
+  GLFWwindow* win = glfwCreateWindow(600,400,"Window title",nullptr,nullptr);
+  assert(win);
+  glfwSetFramebufferSizeCallback(win, resize);  // resize callback
+  glfwSetKeyCallback(win, keyboard);            // keyboard callback
+  glfwSetMouseButtonCallback(win, mousebutton); // mouse button callback
+  
+  glfwMakeContextCurrent(win);
+#ifdef _WIN32
+  if (!gladLoadGL(glfwGetProcAddress)) {
+      printf("Failed to initialize GLAD OpenGL context\n");
+      exit(1);
+  }
+#endif
+  printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+
+  initialize();
+
+  while(!glfwWindowShouldClose(win)) {
+    display(win);
+    glfwSwapBuffers(win);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+  return 0;
+}
+

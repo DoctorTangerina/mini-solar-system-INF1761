114d808b3fc10c4e0ce044a725f050210e6fbd66
lumpleme <108637505+lumpleme@users.noreply.github.com>
2025-10-30 18:42:28 -0300

    tentativa rugosidade shaders

--- shaders/ilum_vert/fragment_texture.glsl
+++ shaders/ilum_vert/fragment_texture.glsl
@@ -19,19 +19,32 @@ uniform float mshi;
 out vec4 color;
 
 uniform sampler2D decal;
+uniform sampler2D normalMap;
+
+vec3 expand (vec3 v)
+{
+  return (v-0.5) * 2;
+}
 
 void main (void)
 {
-  vec3 neye = normalize(f.n);
   vec3 light = normalize(f.l);
+  vec3 normal = expand(vec3(texture(normalMap, f.texcoord)));
   vec3 veye = normalize(f.ve);
 
-  float ndotl = dot(neye,light);
-  color = mamb*lamb + mdif * ldif * max(0,ndotl); 
-  if (ndotl > 0) {
-    vec3 refl = normalize(reflect(-light,neye));
-    color += mspe * lspe * pow(max(0,dot(refl,normalize(-veye))),mshi); 
+  if (light.z > 0)
+  {
+    float ndotl = dot(normal,light);
+    color = mamb*lamb + mdif * ldif * max(0,ndotl); 
+
+    if (ndotl > 0) {
+      vec3 refl = normalize(reflect(-light,normal));
+      color += mspe * lspe * pow(max(0,dot(refl,normalize(-veye))),mshi); 
+    } 
   }
+  else
+    color = mamb*lamb;
+  
 
   color = color * texture(decal,f.texcoord);
 }
--- shaders/ilum_vert/vertex_texture.glsl
+++ shaders/ilum_vert/vertex_texture.glsl
@@ -2,6 +2,7 @@
 
 layout(location = 0) in vec4 coord;
 layout(location = 1) in vec3 normal;
+layout(location = 2) in vec3 tangent;
 layout(location = 3) in vec2 texcoord;
 
 uniform mat4 Mv; 
@@ -21,13 +22,17 @@ void main (void)
 {
   vec3 veye = vec3(Mv*coord);
   vec3 light;
-  if (lpos.w == 0) 
-    light = normalize(vec3(lpos));
-  else 
-    light = normalize(vec3(lpos)-veye); 
-  vec3 neye = normalize(vec3(Mn*vec4(normal,0.0f)));
+
+  vec3 normal_eye = normalize(vec3(Mn * vec4(normal, 0.0)));
+  vec3 tangent_eye = normalize(vec3(Mn * vec4(tangent, 0.0)));
+  vec3 binormal_eye = cross(normal_eye, tangent_eye);
+
+  mat3 rotation = transpose(mat3(tangent_eye,binormal_eye,normal_eye));
+  light = rotation * normalize(vec3(lpos)-veye);   
+  veye = rotation * normalize(-veye); 
+
   
-  v.n = neye;
+  v.n = normal_eye;
   v.l = light;
   v.ve = veye; 
   v.texcoord = texcoord;
